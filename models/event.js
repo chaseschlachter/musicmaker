d7d8f032ab97893e6d9fb6f0279c481547e5b3ba
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-08-19 12:00:32 -0500

    Update event.js

--- models/event.js
+++ models/event.js
@@ -2,24 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-  venue_id: {
+//  artist_id: {
+//    type: Schema.Types.ObjectId,
+//    ref: 'Artist'
+//    index: true
+//  },
+  event_name: String,
+  location: String,
+  description: String,
+  image: String,
+//  event_start: {
+//    type: Date,
+//    required: [true, 'Date & time of event start required']
+//    },
+//  event_end: {
+//    type: Date,
+//    required: [false, 'Date & time of event end required']
+//  },
+//  description: String,
+//  created: {
+//    type: Date,  // Captures both date and time
+//    default: Date.now
+//  },
+  artist: {
     type: Schema.Types.ObjectId,
-    ref: 'Venue',
-    index: true
+    ref: 'Artist'
   },
-  artist_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Artist',
-    index: true
-  },
-  created: {
-    type: Date,  // Captures both date and time
-    default: Date.now
-  },
-  event_time: {
-    type: Date,
-    default: Date.now
-  }
 });
 
-module.exports = mongoose.model('Event', eventSchema); 
+module.exports = mongoose.model('Event', eventSchema);
