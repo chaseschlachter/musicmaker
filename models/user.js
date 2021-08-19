956e93efaf52b80c23ebfc4ff74f69b4b33149f9
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-08-19 12:01:07 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -7,13 +7,26 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true
-    },
-    Class: {
-        type: String,
-        required: true,
-        unique: false
-    }
+      },
+      year_born: {
+          type: Number,
+          required: [true, 'Certain events are 21+']
+      },
+      phone: {
+          type: Number,
+          required: [true, "Needed as we'll text you your check-in validation at events (and nothing more)"]
+      }
+//    gender: {
+//       type: String,
+//        required: true
+//    },
+//    first_name: {
+//        type: String,
+//    },
+//    last_name: {
+//        type: String,
+//    },
 });
 UserSchema.plugin(passportlocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema); 
+module.exports = mongoose.model('User', UserSchema);
