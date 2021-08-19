26f0760bc646c305fe7ceffeac95319609dd228b
chaseschlachter <45405981+chaseschlachter@users.noreply.github.com>
2021-08-19 12:00:08 -0500

    Update artist.js

--- models/artist.js
+++ models/artist.js
@@ -2,18 +2,34 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportlocalMongoose = require('passport-local-mongoose');
 
-const UserSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    Class: {
-        type: String,
-        required: true,
-        unique: false
-    }
+const artistSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  location: {
+    type: String,
+    required: [true, 'Hometown (so you can be paired with local venues)']
+  },
+  genre: {
+    type: String
+  },
+  joined_date: {
+    type: Date,
+    default: Date.now
+  },
+  about: String,
+  size: Number,
+//venmo: String,
 });
-UserSchema.plugin(passportlocalMongoose);
 
-module.exports = mongoose.model('User', UserSchema);
+//artistSchema.virtual('events', {
+//  ref: 'Event', // The model to use
+//  localField: '_id', // Find events where `localField`
+//  foreignField: 'artist', // is equal to `foreignField`
+//  justOne: false // we can have more than 1 event per artist
+//});
+
+artistSchema.plugin(passportlocalMongoose);
+module.exports = mongoose.model('Artist', artistSchema);
